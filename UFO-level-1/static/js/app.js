223336e256e27c376531dd21f7e82ca86d0ff896
tran247 <75042393+tran247@users.noreply.github.com>
2021-05-08 21:45:47 -0400

    looping through data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,3 +7,12 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
+// console log the data from data.js
+
+console.log(data);
+
+// Loop through data and console log ea weather report 
+
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting)
+});
