bf367305c167d4c68f4a8552c48ca0eb3a2f6855
tran247 <75042393+tran247@users.noreply.github.com>
2021-05-08 21:58:10 -0400

    added table row and used Object.entries

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,8 +11,21 @@ var tbody = d3.select("tbody");
 
 console.log(data);
 
-// Loop through data and console log ea weather report 
+// Loop through data and console log ea ufo sighting 
 
 data.forEach(function(ufoSighting) {
-    console.log(ufoSighting)
+    console.log(ufoSighting);
+
+
+    // append one table row 'tr' for ea ufo sighting 
+
+    var row =tbody.append("tr");
+
+    // use 'Object.entries' to console. log ea sighting
+
+    Object.defineProperties(ufoSighting).forEach(function([key, value]) {
+    console.log(key, value);
+    });
 });
+
+
